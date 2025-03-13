abeff5a3dc5723481ea9bc7fab02ee64db140f19
amirbeek <Amirbek.shomurodov01@gmail.com>
2025-03-13 16:40:27 +0000

    updated
    SEO  component updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,12 @@ import {
 export default function IndexPage() {
   return (
       <>
-        <Seo title="Portfolio Minimal" />
+        <Seo
+            title="Amirbek's Portfolio Website"
+            description={'Explore the innovative portfolio of Amirbek Shomurodov, a software engineer with specialized expertise in test automation, web and backend development, and data science. With a Bachelor’s degree in Computer Science from Brunel University London, Amirbek has contributed to significant projects like the development of a COVID-19 detection application and a robust e-commerce platform for emerging designers. Discover how his passion for technology and commitment to quality drives him to deliver high-performance solutions that enhance user experiences. Dive into a portfolio that not only showcases technical proficiency across Python, JavaScript, and more but also highlights his leadership in collaborative projects and community-oriented volunteer work.'}
+            lang="en"
+            image="/favicon.png"
+        />
         <Page useSplashScreenAnimation>
             <HeroSection sectionId="hero" />
             <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Blog', 'Medium']} />
